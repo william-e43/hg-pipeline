e72cb5e58e9846a8399738800098f60316aa3f4d
William <williameliason8@gmail.com>
2021-06-05 17:20:45 -0500

    removed next button for 3way questions

--- src/components/ButtonTemplates.jsx
+++ src/components/ButtonTemplates.jsx
@@ -3,7 +3,10 @@ import {SubmitButton, StyledLink, Button, ButtonWrapper} from '../style';
 
 export const YNQuestion = ({yPath, nPath, currentPath, backPath, yButtonText, nButtonText}) => {
 
-  const [path, setPath] = useState(currentPath)
+  //the below code has been rendered useless when I removed the 'next' button and had the app route on y/n selection
+  //i'm keeping it just in case however, and i am not commenting it out so that I don't have to remove references to it
+
+  //const [path, setPath] = useState(currentPath)
   const [a1, seta1] = useState();
   const [a2, seta2] = useState();
 
@@ -12,12 +15,12 @@ export const YNQuestion = ({yPath, nPath, currentPath, backPath, yButtonText, nB
       case 'yes':
         seta1(true);
         seta2(false);
-        setPath({pathname: yPath, state: {from: currentPath}});
+        //setPath({pathname: yPath, state: {from: currentPath}});
         break;
       case 'no':
         seta1(false);
         seta2(true);
-        setPath({pathname: nPath, state: {from: currentPath}});
+        //setPath({pathname: nPath, state: {from: currentPath}});
         break;
       default:
         alert('Choose a valid option');
@@ -49,7 +52,7 @@ export const YNQuestion = ({yPath, nPath, currentPath, backPath, yButtonText, nB
 export const ThreeWayQuestion = (props) => {
   const {firstPath, secondPath, thirdPath, currentPath, backPath, fPathText, sPathText, tPathText} = props.props;
 
-  const [path, setPath] = useState(currentPath);
+  //const [path, setPath] = useState(currentPath);
   const [a1, seta1] = useState();
   const [a2, seta2] = useState();
   const [a3, seta3] = useState();
@@ -60,19 +63,19 @@ export const ThreeWayQuestion = (props) => {
         seta1(true);
         seta2(false);
         seta3(false);
-        setPath({pathname: firstPath, state: {from: currentPath}});
+        //setPath({pathname: firstPath, state: {from: currentPath}});
         break;
       case 'ans2':
         seta1(false);
         seta2(true);
         seta3(false);
-        setPath({pathname: secondPath, state: {from: currentPath}});
+        //setPath({pathname: secondPath, state: {from: currentPath}});
         break;
       case 'ans3':
         seta1(false);
         seta2(false);
         seta3(true);
-        setPath({pathname: thirdPath, state: {from: currentPath}});
+        //setPath({pathname: thirdPath, state: {from: currentPath}});
         break;
       default:
         alert('Please choose a valid input');
@@ -81,20 +84,26 @@ export const ThreeWayQuestion = (props) => {
   return (
     <div>
       <ButtonWrapper>
-        <Button state={a1} onClick={() => onClick('ans1')}>{fPathText}</Button>
+        <StyledLink to={{pathname: firstPath, state: {from: currentPath}}}>
+          <Button state={a1} onClick={() => onClick('ans1')}>{fPathText}</Button>
+        </StyledLink>
       </ButtonWrapper>
       <ButtonWrapper>
+      <StyledLink to={{pathname: secondPath, state: {from: currentPath}}}>
         <Button state={a2} onClick={() => onClick('ans2')}>{sPathText}</Button>
+      </StyledLink>
       </ButtonWrapper>
       <ButtonWrapper>
+      <StyledLink to={{pathname: thirdPath, state: {from: currentPath}}}>
         <Button state={a3} onClick={() => onClick('ans3')}>{tPathText}</Button><br></br>
+      </StyledLink>
       </ButtonWrapper><br></br>
       <ButtonWrapper>
         <StyledLink to={backPath}><SubmitButton>Back</SubmitButton></StyledLink>
       </ButtonWrapper>
-      <ButtonWrapper>
+      {/* <ButtonWrapper>
         <StyledLink to={path}><SubmitButton>Next</SubmitButton></StyledLink>
-      </ButtonWrapper>
+      </ButtonWrapper> */}
     </div>
   )
 }
